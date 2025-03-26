415be8a5b977932567d6d9e29ac5b404fc8f7055
Klara <k.vesel8@seznam.cz>
2025-03-26 13:33:05 +0100

    Added SOM files: usersSom.ts, coverPhotosSom.ts, booksSom.ts, authorsSom.ts, activitiesSom.ts

---
+++ src/som/usersSom.ts
@@ -0,0 +1,15 @@
+import { TestType, PlaywrightTestArgs, PlaywrightTestOptions, PlaywrightWorkerArgs, PlaywrightWorkerOptions, APIRequest, APIResponse} from '@playwright/test'
+
+export class UsersSom {
+
+    protected _apiResponse: APIResponse
+    protected _test: TestType <PlaywrightTestArgs & PlaywrightTestOptions, PlaywrightWorkerArgs & PlaywrightWorkerOptions>
+
+    constructor (
+        apiResponse:APIResponse,
+        test: TestType <PlaywrightTestArgs & PlaywrightTestOptions, PlaywrightWorkerArgs & PlaywrightWorkerOptions>
+    ) {
+        this._apiResponse = apiResponse
+        this._test = test
+    }
+}
